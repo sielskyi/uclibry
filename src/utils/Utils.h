7fd7b0ac809d16959893db3b6276f0a3e6be07f5
Leonid Sielskyi <sielskyi.leonid@gmail.com>
2016-12-15 23:39:00 +0300

    Rename and reformatting of file.

--- src/utils/Utils.h
+++ src/utils/Utils.h
@@ -1,8 +1,8 @@
-/** \file utils.h
+/** \file Utils.h
 *   \brief   General utilities. Header file.
 *
 *   \author     sielskyi (SLL)
-*   \date       2016/11/21
+*   \date       2016/12/15
 *   \version    01.01
 *
 *   \section    sect1   Descriptions
@@ -15,7 +15,7 @@
 
 /* Include files **************************************************************/
 
-#include "clibry.h"			///< CLibry main header file.
+#include "uclibry.h"			///< uCLibry main header file.
 
 /* Constants ******************************************************************/
   
@@ -47,12 +47,33 @@
 
 /* Public Functions ***********************************************************/
 
-void Utils_SwapBytes(uint8_t* pb1, uint8_t* pb2);
-void Utils_SetUInt16ToMem(void* mem, uint16_t val);
-void Utils_SetUInt32ToMem(void* mem, uint32_t val);
+/** \fn void Utils_swapBytes(uint8_t* pb1, uint8_t* pb2)
+	Swap two bytes through the pointers.
+    Public function.
+    \param  pb1     - pointer to the first byte.
+    \param  pb2     - pointer to the second byte.
+    \return void.
+*/
+void Utils_swapBytes(uint8_t* pb1, uint8_t* pb2);
+
+/** \fn void Utils_setUInt16ToMem(void* mem, uint16_t val)
+	Set UInt16 value to memory.
+    Public function.
+    \param  mem     - pointer to destination memory.
+    \param  val     - UInt16 value to set.
+    \return void.
+*/
+void Utils_setUInt16ToMem(void* mem, uint16_t val);
+
+/** \fn void Utils_setUInt32ToMem(void* mem, uint32_t val)
+	Set UInt32 value to memory.
+    Public function.
+    \param  mem     - pointer to destination memory.
+    \param  val     - UInt32 value to set.
+    \return void.
+*/
+void Utils_setUInt32ToMem(void* mem, uint32_t val);
 
 /******************************************************************************/
 #endif  // __UCLIBRY_UTILS_H
-
-
-
+/* End of file ****************************************************************/
